7fd90638dc6fb2da4afec3ae9b9b3286d0cc15bb
Keana Moore <keanalane7@gmail.com>
2020-04-30 22:15:00 -0700

    Add files via upload

---
+++ IterationProject/IterationProject/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  IterationProject
+//
+//  Created by Keana  Moore on 4/30/20.
+//  Copyright © 2020 Keana  Moore. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
